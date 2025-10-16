d59389da96aae522a853f5f54cc9139c317e84f7
TheBananaProtocol <tjthegoatman25@gmail.com>
2025-10-16 09:51:28 -0400

    week 9 work

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -6,7 +6,7 @@ print("********** Ch 5 Exercise 1 **********")
 
 # Do your work for Exercise 1 here
 
-print("Ch 5 Exercise 1: Not implemented") # Delete this line when you write your code!
+
 
 
 
@@ -22,12 +22,30 @@ print("********** Ch 5 Exercise 3 **********")
 
 # Do your work for Exercise 3 here.
 
-print("Ch 5 Exercise 3: Not implemented") # Delete this line when you write your code!
+def is_tringle(a,b,c):
+    print('is_tringle()',a,b,c)
+    if a>= b + c:
+        print("not a tringle")
+    elif b>= c + a:
+        print("it's not a tringle")
+    elif c>= a + b:
+        print("no tringle")
+    
+    else:
+        print("ts a tringle")
 
 
+a = float(input('how long is side a'))
+print('a is', a, type(a))
+b = float(input('how long is side b'))
+print('b is', b, type(b))
+c = float(input('how long is side c'))
+print('c is', c, type(c))
+
+is_tringle(a, b, c)
 
 print("********** Ch 5 Exercise 4 **********")
 
 # Do your work for Exercise 4 here.
 
-print("Ch 5 Exercise 4: Not implemented") # Delete this line when you write your code!
+
--- chapters/05_2_homework/hw1_1.py
+++ chapters/05_2_homework/hw1_1.py
@@ -6,15 +6,31 @@ print("********** Homework 1 Exercise 1 **********")
 
 # Do your work for Exercise 1 here
 
-print("Homework 1 Exercise 1: Not implemented") # Delete this line when you write your code!
+def print_tic_tac_foot():
+    v_row = (' '*2 + '|')*2 + ' '*2
+    h_row = '-'*8
 
+    print(v_row)
+    print(h_row)
+    print(v_row)
+    print(h_row)
+    print(v_row)
+print_tic_tac_foot()
 
 
 print("********** Homework 1 Exercise 2 **********")
 
 # Do your work for Excercise 2 here
 
-print("Homework 1 Exercise 2: Not implemented") # Delete this line when you write your code!
+
+name = input('whatis your name? ')
+print('Hi, ' + name + '!')
+if input == ('james'):
+    print("locking on your pasition")
+
+
+
+    
 
 
 
