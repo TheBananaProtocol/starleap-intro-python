ff2c77b248991ed750d6e4d156ec346fd958a03c
TheBananaProtocol <tjthegoatman25@gmail.com>
2025-11-20 09:54:59 -0500

    week14

---
+++ chapters/04_1_lesson/mypolygon.py
@@ -0,0 +1,55 @@
+import turtle
+
+
+
+
+#fu the turtle
+
+fu = turtle.Turtle()
+fu.shape('turtle')
+fu.color('blue', 'green')
+fu.speed(1)
+
+
+
+print('pos', fu.pos())
+fu.penup()
+fu.goto(-200, -100)
+fu.color('green')
+fu.pendown()
+fu.write("WEEEEEEEEEEEEEE!!!!")
+
+
+
+fu.fd(300)
+fu.lt(600)
+fu.fd(600)
+
+
+#gu the turtle
+
+gu = turtle.Turtle()
+gu.shape('turtle')
+gu.color('blue')
+gu.speed(1)
+
+
+
+print('pos', gu.pos())
+gu.penup()
+gu.goto(-100, -100)
+gu.color('blue')
+gu.pendown()
+gu.write("WEEEEEEEEEEEEEE!!!!")
+
+
+
+gu.fd(455)
+gu.lt(600)
+gu.fd(520)
+
+
+
+
+turtle.mainloop()
+
