0726947a70b7f2c4bc49aeee5780b91196ca3578
TheBananaProtocol <tjthegoatman25@gmail.com>
2026-03-05 09:53:27 -0500

    week 15

--- chapters/08_1_lesson/play.py
+++ chapters/08_1_lesson/play.py
@@ -1,7 +1,7 @@
 
-fruit = 'tacocat'
+fruit = '🤣🤣🤔☺🐛'
 
-index =0
+index =4
 while index < len(fruit):
     letter=fruit[index]
     print(letter)
@@ -9,3 +9,28 @@ while index < len(fruit):
 
 
 
+prefixes = 'TKLMNOPQ'
+suffix = 'elker'
+
+for letter in prefixes:
+    print (letter + suffix)
+
+
+def find(word, letter):
+    index = 0
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1
+
+print(find)
+
+word = banana
+
+if word < 'banana':
+    print('Your word, ' + word + ', comes before banana.')
+elif word > 'banana':
+    print('Your word, ' + word + ', comes after banana.')
+else:
+    print('All right, bananas.')
