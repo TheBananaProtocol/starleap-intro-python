9e08e7177e455369f3dd451cc0d27154e61fd7e3
TheBananaProtocol <tjthegoatman25@gmail.com>
2026-03-26 09:52:23 -0400

    week nimsyes

--- chapters/08_2_game/nims.py
+++ chapters/08_2_game/nims.py
@@ -4,13 +4,13 @@
 
 min_stones = 1
 max_stones = 5
-pile = 100
-
+pile = 10
+player = 1
 
 
 def valid_answer():
     while True:
-        guess_text=input("enter your guess: ")
+        guess_text=input(f"player {player} how many stones do you want take ")
         try:
             guess = int(guess_text)
             if guess > max_stones or guess < min_stones:
@@ -25,7 +25,20 @@ def valid_answer():
 
 
 def play_nims(pile, max_stones):
-    answer = valid_answer
+    global player
+    print (f"there are {pile} stones" )
+    while pile > 0:
+        answer = valid_answer()
+        pile -= answer
+        if player == 1:
+            player = 2
+        else:
+            player = 1
+    if pile > 0 and player == 1:
+        print("congrates player 1 you just beat a 4 year old girl in a too too👧")
+    else:
+        print("yo playa 2 you stole the cheder from playa 1")
+
 
 
     
