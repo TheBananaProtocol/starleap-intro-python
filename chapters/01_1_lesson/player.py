e6019479486762b6657f612e767d87cb545342b4
TheBananaProtocol <tjthegoatman25@gmail.com>
2026-08-20 09:51:10 -0400

    week william

---
+++ chapters/01_1_lesson/player.py
@@ -0,0 +1,39 @@
+
+
+
+you= 'welker'
+
+age = 26*95
+
+print( you, 'is', (age),' years old'  )
+print( you, 'is', (age*7), 'in dog years')
+print( you, 'is going to be', (age+7),'in 7 years')
+print( you, 'is',(age+45), 'if you added 45 to age')
+
+
+price = 23
+tax_rate = 0.7
+total= price + price *tax_rate
+
+print('total', total)
+
+
+import math 
+degrees =45
+radians = degrees / 180 * math.pi
+print(math.sin(radians))
+
+value = math.sqrt(3)
+print(value)
+
+
+print (5/2)
+print(5//2)
+
+class_min = 90
+hours =class_min // 60 
+minutes = class_min - (hours *60)
+
+print(f"class {hours} hours and {minutes} min long")
+
+
